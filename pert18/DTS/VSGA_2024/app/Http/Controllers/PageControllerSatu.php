8866069ae054d69f6c9d3d4b862d6bf1fcb21d4f
Erinthia16 <erinthiapratiwi167@gmail.com>
2024-08-10 16:57:49 +0700

    buat fungsi di dalamnya

--- pert18/DTS/VSGA_2024/app/Http/Controllers/PageControllerSatu.php
+++ pert18/DTS/VSGA_2024/app/Http/Controllers/PageControllerSatu.php
@@ -7,4 +7,10 @@ use Illuminate\Http\Request;
 class PageControllerSatu extends Controller
 {
     //
+    public function index(){
+        return view('welcome');
+    }
+    public function satu(){
+        return 'data pertama';
+    }
 }
