4af58b9d0c94e1b349c956dd231f5c8f6cc1f6d8
Erinthia16 <erinthiapratiwi167@gmail.com>
2024-08-11 10:06:54 +0700

    buat migrasi table m_kategori dan m_user

--- PWL-POS/database/migrations/2024_08_11_020907_create_m_level_table.php
+++ PWL-POS/database/migrations/2024_08_11_020907_create_m_level_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
 
 return new class extends Migration
 {
@@ -11,10 +12,12 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up():void
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+        Schema::create('m_level', function (Blueprint $table) {
+            $table->id('level_id');
+            $table->string('level_kode', 10)->Unique();
+            $table->string('level_nama', 100);
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('m_level');
     }
 };
