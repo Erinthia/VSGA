474a7a0d8db92e5ed1103b107b7fdb585e67db22
Erinthia16 <erinthiapratiwi167@gmail.com>
2024-08-11 10:40:02 +0700

    sama kayak awal

--- PWL-POS/database/migrations/2024_08_11_032803_create_m_level_table.php
+++ PWL-POS/database/migrations/2024_08_11_032803_create_m_level_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
 
 return new class extends Migration
 {
@@ -11,14 +12,12 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up():void
     {
-        Schema::create('m_user', function (Blueprint $table) {
-            $table->id('user_id');
-            $table->unsignedBigInteger('level_id')->index();
-            $table->string('username', 20)->unique();
-            $table->string('nama', 100);
-            $table->string('password');
+        Schema::create('m_level', function (Blueprint $table) {
+            $table->id('level_id');
+            $table->string('level_kode', 10)->Unique();
+            $table->string('level_nama', 100);
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('m_user');
+        Schema::dropIfExists('m_level');
     }
 };
