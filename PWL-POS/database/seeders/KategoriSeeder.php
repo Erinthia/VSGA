2aaa5caf09bd7b181c14dc485bcbe44c9918bc63
Erinthia16 <erinthiapratiwi167@gmail.com>
2024-08-11 12:34:21 +0700

    buat seeder kategori

--- PWL-POS/database/seeders/KategoriSeeder.php
+++ PWL-POS/database/seeders/KategoriSeeder.php
@@ -4,16 +4,23 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class KategoriSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      *
-     * @return void
      */
-    public function run()
+    public function run():void
     {
-        //
+        $data =[
+            ['kategori_id' => 1, 'kategori_kode' => 'MKN', 'kategori_nama' => 'Makanan'],
+            ['kategori_id' => 2, 'kategori_kode' => 'MNM', 'kategori_nama' => 'Minuman'],
+            ['kategori_id' => 3, 'kategori_kode' => 'DSRT', 'kategori_nama' => 'Desert'],
+            ['kategori_id' => 4, 'kategori_kode' => 'ESC', 'kategori_nama' => 'Es Cream'],
+            ['kategori_id' => 5, 'kategori_kode' => 'ESB', 'kategori_nama' => 'Es Buah'],
+        ];
+        DB::table('m_kategori')->insert($data);
     }
 }
