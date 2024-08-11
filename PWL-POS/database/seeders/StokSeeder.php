b66a5befc7b5716df345714c5f6addab9d746890
Erinthia16 <erinthiapratiwi167@gmail.com>
2024-08-11 13:43:42 +0700

    buat seeder stok

--- PWL-POS/database/seeders/StokSeeder.php
+++ PWL-POS/database/seeders/StokSeeder.php
@@ -15,27 +15,17 @@ class StokSeeder extends Seeder
     public function run()
     {
         $data = [
-            [
-            'stok_id' => 1,
-            'barang_id' => 1,
-            'username' => 'admin',
-            'nama' => 'Administrator',
-            'password' => Hash::make('12345')
-        ],
-        
-            
+            ['stok_id' => 1, 'barang_id' => 1, 'user_id' => 1, 'stok_tanggal' => '2021-01-01 08:00:00', 'stok_jumlah' => 100],
+            ['stok_id' => 2, 'barang_id' => 2, 'user_id' => 1, 'stok_tanggal' => '2021-02-10 08:00:00', 'stok_jumlah' => 150],
+            ['stok_id' => 3, 'barang_id' => 3, 'user_id' => 1, 'stok_tanggal' => '2021-03-20 08:00:00', 'stok_jumlah' => 200],
+            ['stok_id' => 4, 'barang_id' => 4, 'user_id' => 2, 'stok_tanggal' => '2021-01-5 08:00:00', 'stok_jumlah' => 210],
+            ['stok_id' => 5, 'barang_id' => 5, 'user_id' => 2, 'stok_tanggal' => '2021-02-15 08:00:00', 'stok_jumlah' => 90],
+            ['stok_id' => 6, 'barang_id' => 6, 'user_id' => 2, 'stok_tanggal' => '2021-03-25 08:00:00', 'stok_jumlah' => 50],
+            ['stok_id' => 7, 'barang_id' => 7, 'user_id' => 3, 'stok_tanggal' => '2021-04-01 08:00:00', 'stok_jumlah' => 100],
+            ['stok_id' => 8, 'barang_id' => 8, 'user_id' => 3, 'stok_tanggal' => '2021-05-10 08:00:00', 'stok_jumlah' => 200],
+            ['stok_id' => 9, 'barang_id' => 9, 'user_id' => 3, 'stok_tanggal' => '2021-06-01 08:00:00', 'stok_jumlah' => 150],
+            ['stok_id' => 10, 'barang_id' => 10, 'user_id' => 3, 'stok_tanggal' => '2021-05-15 08:00:00', 'stok_jumlah' => 250],
         ];
-        DB::table('m_user')->insert($data);
-    }
+        DB::table('t_stok')->insert($data);
     }
 }
-// $table->id('stok_id');
-// $table->unsignedBigInteger('barang_id')->index();
-// $table->unsignedBigInteger('user_id')->index();
-// $table->dateTime('stok_tanggal');
-// $table->integer('stok_jumlah');
-// $table->timestamps();
-
-// $table->foreign('barang_id')->references('barang_id')->on('m_barang');
-// $table->foreign('user_id')->references('user_id')->on('m_user');
-// });
