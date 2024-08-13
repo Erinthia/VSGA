f4851b15dc9d6719789929fb600cd8563f188399
Erinthia16 <erinthiapratiwi167@gmail.com>
2024-08-13 07:26:11 +0700

    modifikasi kategoricontroller untuk meng-update data table m_kategori

--- PWL-POS/app/Http/Controllers/KategoriController.php
+++ PWL-POS/app/Http/Controllers/KategoriController.php
@@ -9,12 +9,17 @@ class KategoriController extends Controller
 {
     public function index()
     {
-        $data=[
-            'kategori_kode'=>'SNK',
-            'kategori_nama'=>'Snack/makanan ringan',
-            'created_at'=>now(),
-        ];
-        DB::table('m_kategori')->insert($data);
-        return 'Data berhasil ditambah';
+        //menambah data
+    //     $data=[
+    //         'kategori_kode'=>'SNK',
+    //         'kategori_nama'=>'Snack/makanan ringan',
+    //         'created_at'=>now(),
+    //     ];
+    //     DB::table('m_kategori')->insert($data);
+    //     return 'Data berhasil ditambah';
+
+    //update data
+    $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama'=>'Cemilan']);
+    return 'Data berhasil diupdate, jumlah data yang terupdate' . $row . 'baris';
     }
 }
