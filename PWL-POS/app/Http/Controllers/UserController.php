78a31400eaff57e7378e9e56c2b69a4bc1691f7b
Erinthia16 <erinthiapratiwi167@gmail.com>
2024-08-14 06:42:47 +0700

    modifikasi usercontroller untuk update data

--- PWL-POS/app/Http/Controllers/UserController.php
+++ PWL-POS/app/Http/Controllers/UserController.php
@@ -10,14 +10,19 @@ class UserController extends Controller
 {
     public function index(){
         //menambahkan data menggunakan ORM
-        $data =[
-            'username'=> 'erin',
-            'nama'=> 'eirn',
-            'password'=> Hash::make('12345'),
-            'user_id'=> 4,
-            'level_id'=> 1
+        // $data =[
+        //     'username'=> 'erin',
+        //     'nama'=> 'eirn',
+        //     'password'=> Hash::make('12345'),
+        //     'user_id'=> 4,
+        //     'level_id'=> 1
+        // ];
+        // UserModel::insert($data);
+
+        $data=[
+            'nama'=>'dinda',
         ];
-        UserModel::insert($data);
+        UserModel::where('username', 'erin')->update($data);
 
         //akses model usermodel
         $user=UserModel::all();
