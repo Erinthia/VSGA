80fe269f2579d3532ea8a778779918a4e00a0530
Erinthia16 <erinthiapratiwi167@gmail.com>
2024-08-14 07:04:42 +0700

    buat fungsi pada anggota controller untuk melihat data

--- PWL-POS/app/Http/Controllers/AnggotaController.php
+++ PWL-POS/app/Http/Controllers/AnggotaController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Anggota;
 
 class AnggotaController extends Controller
 {
     //
+    public function cekObject(){
+        $anggota = new Anggota;
+
+        dump($anggota);
+    }
 }
