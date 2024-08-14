60367cafc2066b0acf66808bfc7742bfd68fc5c2
Erinthia16 <erinthiapratiwi167@gmail.com>
2024-08-14 07:29:42 +0700

    insert data lebih dari 1

--- PWL-POS/app/Http/Controllers/AnggotaController.php
+++ PWL-POS/app/Http/Controllers/AnggotaController.php
@@ -15,10 +15,10 @@ class AnggotaController extends Controller
     }
     public function insert(){
         $anggota = new Anggota;
-        $anggota->nip = "22317405";
-        $anggota->nama = "dinda";
-        $anggota->tanggal_lahir = "2002-08-13";
-        $anggota->nilai = "3.2";
+        $anggota->nip = "22317406";
+        $anggota->nama = "erin";
+        $anggota->tanggal_lahir = "2002-07-13";
+        $anggota->nilai = "3.5";
         $anggota->save();
         echo"Data berhasil disimpan";
     }
