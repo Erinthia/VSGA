b7265fda3b6d899302cf7a41abb42f996be872e3
Erinthia16 <erinthiapratiwi167@gmail.com>
2024-08-18 21:22:09 +0700

    buat file blade sesuai navbar

--- pert23/routes/web.php
+++ pert23/routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('home');
 });
 
 Route::get('/team', function () {
@@ -37,6 +37,4 @@ Route::get('/report', function () {
     return view('report');
 });
 
-Route::get('/halaman', function () {
-    return view('halaman');
-});
+
